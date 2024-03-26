f47f4164aaa225848f709a36720a9e17b826a523
yevhen1979kharkov <157107712+yevhen1979kharkov@users.noreply.github.com>
2024-03-26 10:40:38 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp10
+    {
+    internal class Program
+        {
+        static void Main(string[] args)
+            {
+            int resultFromTread = 0;
+            Thread thread = new Thread(() =>
+            {
+                Thread.Sleep(5000);
+                resultFromTread = 100;
+            });
+            thread.Start();
+            Console.WriteLine(resultFromTread);
+
+            }
+        }
+    public class Q {
+  
+
+    }
+    public class Currencies {
+        public string symbol { get; set; }
+    }
+    }
