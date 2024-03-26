f6dc25665f20d6da0c9bdb1a13b4040821102077
yevhen1979kharkov <157107712+yevhen1979kharkov@users.noreply.github.com>
2024-03-26 10:42:07 +0200

    qw

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace ConsoleApp9
+    {
+    internal class Program
+        {
+        static void Main(string[] args)
+            {
+            Console.WriteLine(  String.IsNullOrWhiteSpace(String.Empty) );
+           
+            }
+        }
+    }
